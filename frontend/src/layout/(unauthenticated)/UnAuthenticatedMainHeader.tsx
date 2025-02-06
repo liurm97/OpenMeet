2bfb914fa0c81853ee78ee4f1c605d5cc4f73b6f
bobby.liu <fellowe5@hotmail.com>
2025-02-06 11:59:31 +0800

    1. Added basic google auth flow to UnauthenticatedMainHeader.tsx

--- frontend/src/layout/(unauthenticated)/UnAuthenticatedMainHeader.tsx
+++ frontend/src/layout/(unauthenticated)/UnAuthenticatedMainHeader.tsx
@@ -1,8 +1,34 @@
 import { Link } from "react-router-dom";
 import CalendarLogo from "../../assets/calendar_icon.svg?react";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useGoogleLogin } from "@react-oauth/google";
+import { googleLogout } from "@react-oauth/google";
+import { useState } from "react";
 
 const UnAuthenticatedMainHeader = () => {
+  const [signInState, setSignInState] = useState<boolean>(false);
+
+  const login = useGoogleLogin({
+    onSuccess: (successResponse) => {
+      const accessToken = successResponse.access_token;
+      console.log(`accessToken:: ${accessToken}`);
+      setSignInState(true);
+    },
+    onError: (errorResponse) => {
+      console.log(`Failed\nerrorResponse::${JSON.stringify(errorResponse)}`);
+    },
+  });
+
+  // states
+
   return (
     <header className="border-b">
       <div className="container mx-auto px-4 h-16 flex items-center justify-between">
@@ -17,12 +43,35 @@ const UnAuthenticatedMainHeader = () => {
           <Link to="#faq" className="text-sm hover:text-gray-600">
             FAQ
           </Link>
-          <Button
+          {signInState == false ? (
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button
+                  variant="default"
+                  className="bg-black text-white hover:bg-gray-800"
+                >
+                  Sign in
+                </Button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Sign in</DialogTitle>
+                  <Button onClick={() => login()}>Continue With Google</Button>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+          ) : (
+            <h1>Signed in</h1>
+          )}
+          {/* <Button
             variant="default"
             className="bg-black text-white hover:bg-gray-800"
+            onClick={() => {
+
+            }}
           >
             Sign in
-          </Button>
+          </Button> */}
         </nav>
       </div>
     </header>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./pages/(shared)/ErrorPage.tsx";
 import UnAuthenticatedHomePage from "./pages/(unauthenticated)/UnAuthenticatedHomePage.tsx";
 import UnAuthenticatedAvailabilityPage from "./pages/(unauthenticated)/UnAuthenticatedAvailabilityPage.tsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import * as client_secret from "/Users/bobby/uol/final_project/dev/OpenMeet/frontend/client_secret.json";
 
 const router = createBrowserRouter([
   {
@@ -24,8 +26,14 @@ const router = createBrowserRouter([
   },
 ]);
 
+const clientId = client_secret.web.client_id;
+
+console.log(`clientId:: ${clientId}`);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <GoogleOAuthProvider clientId={clientId}>
+      <RouterProvider router={router} />
+    </GoogleOAuthProvider>
   </StrictMode>
 );
